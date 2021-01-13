0462c291b28cb14494e9248bae5aeea21d3eb189
Zongyoujifeng <m18797818385@163.com>
2021-01-13 12:14:50 +0800

    First Commit

---
+++ basiccode/src/com/zongyoujifeng/day01/initialclass/FirstJava.java
@@ -0,0 +1,14 @@
+package com.zongyoujifeng.day01.initialclass;
+
+/**
+ * @author 10451
+ * @version 1.0
+ * @ClassName FirstJava
+ * @description HelloJava入门程序
+ * @date 2021/1/13 12:13
+ */
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("First Java program.Hello Java!");
+    }
+}
