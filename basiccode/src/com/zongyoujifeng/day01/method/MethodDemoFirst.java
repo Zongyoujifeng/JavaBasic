f8cb6aa5a8ae974abf45a47ff666265e1f293592
Zongyoujifeng <m18797818385@163.com>
2021-01-13 12:24:01 +0800

    SecondCommit

---
+++ basiccode/src/com/zongyoujifeng/day01/method/MethodDemoFirst.java
@@ -0,0 +1,18 @@
+package com.zongyoujifeng.day01.method;
+
+/**
+ * @author 10451
+ * @version 1.0
+ * @ClassName MethodDemoFirst
+ * @description 第一个Java方法Demo
+ * @date 2021/1/13 12:22
+ */
+public class MethodDemoFirst {
+    public static void methodDemo() {
+        System.out.println("第一个Java方法");
+    }
+    public static void main(String[] args) {
+        System.out.println("调用第一个Java方法");
+        methodDemo();
+    }
+}
