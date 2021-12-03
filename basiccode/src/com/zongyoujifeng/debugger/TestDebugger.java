c20cb8b29e2a67cc7dac422472a2ed0a6344eaa7
Zongyoujifeng <m18797818385@163.com>
2021-12-03 16:00:10 +0800

    修改

--- basiccode/src/com/zongyoujifeng/debugger/TestDebugger.java
+++ basiccode/src/com/zongyoujifeng/debugger/TestDebugger.java
@@ -3,7 +3,7 @@ package com.zongyoujifeng.debugger;
 import java.util.Arrays;
 
 /**
- * @author 10451
+ * @author Zongyoujifeng
  * @version 1.0
  * @ClassName TestDebugger
  * @description 调试 F8不会进入方法内部  F7会进入自定义方法中 ALT+Shift+F7第三种会进入方法内部中包括系统内部方法 Shift+F8跳出方法 F9进入下一个断点，如果没有就直接运行到程序结束
